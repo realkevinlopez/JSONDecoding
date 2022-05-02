a2f885ef83e5ddfd38ce178f8dc7ab801bdd7fea
realkevinlopez <51682696+realkevinlopez@users.noreply.github.com>
2022-05-02 08:17:05 -0400

    updates to assignment

---
+++ JSONDecoding/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  JSONDecoding
+//
+//  Created by Kevin Lopez on 5/1/22.
+//
+
+import Foundation
+
+struct Pokemon {
+    var damage_relations: [Relations]
+    var double_damage_to
+    
+}
+
+struct double_damage_from {
+    
+}
