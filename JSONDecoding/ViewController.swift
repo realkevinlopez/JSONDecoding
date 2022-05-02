16ef657d09c355143ea93b50c58821237bac1a82
realkevinlopez <51682696+realkevinlopez@users.noreply.github.com>
2022-05-01 22:47:11 -0400

    Initial Commit

---
+++ JSONDecoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONDecoding
+//
+//  Created by Kevin Lopez on 5/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
